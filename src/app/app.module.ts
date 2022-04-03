f8206db1902f6b483c2b2a10b97c19b0a51a2368
Fenyep <tenordubois10@gmail.com>
2022-04-03 03:50:45 +0100

    Karcial Store: E-commerce application landing page with Anglular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,31 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { FooterComponent } from './footer/footer.component';
+import { SponsorComponent } from './sponsor/sponsor.component';
+import { SupportComponent } from './support/support.component';
+import { InformationComponent } from './information/information.component';
+import { MenuComponent } from './menu/menu.component';
+import { ProductsComponent } from './products/products.component';
+import { ProductCardComponent } from './product-card/product-card.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    FooterComponent,
+    SponsorComponent,
+    SupportComponent,
+    InformationComponent,
+    MenuComponent,
+    ProductsComponent,
+    ProductCardComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
